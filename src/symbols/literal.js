82a3568f99d8d95d9b8898a1ddea4e7f87e24e0e
Tomasz Lenarcik <apendua@gmail.com>
2018-01-23 23:22:06 +0100

    Remove unnecessary code

--- src/symbols/literal.js
+++
@@ -1,9 +0,0 @@
-import { SYMBOL_LITERAL } from '../core/constants.js';
-
-export function literal(gramar) {
-  gramar
-    .define(SYMBOL_LITERAL)
-    .ifUsedAsPrefix((parse, token) => ({
-      value: token.value,
-    }));
-}
